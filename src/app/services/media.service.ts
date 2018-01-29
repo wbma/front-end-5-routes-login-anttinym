2405f692b36551ad111316e0652b21a3bc2484be
anttinym <nyman.antti@gmail.com>
2018-01-29 20:05:19 +0200

    login page submitting info with api, works

---
+++ src/app/services/media.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+
+@Injectable()
+export class MediaService {
+
+  username: string;
+  password: string;
+  apiUrl = 'http://media.mw.metropolia.fi/wbma/';
+
+  constructor(private http: HttpClient) { }
+
+  login() {
+   const body = {
+     username: this.username,
+     password: this.password
+   };
+
+   const settings = {
+     headers: new HttpHeaders().set('Content-Type', 'application/json')
+   };
+
+   this.http.post(this.apiUrl + 'login', body, settings).subscribe(response => {
+     console.log(response['token']);
+   });
+  }
+
+}
